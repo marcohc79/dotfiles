eaf18f06560369692973f4260c89b6a4d4921b35
Marco Huamani <mhcaballero79@gmail.com>
2023-09-25 20:41:24 -0300

    [lvim] with dvorak

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -190,3 +190,9 @@ vim.api.nvim_create_autocmd("BufEnter", {
   command = "setlocal tabstop=8 shiftwidth=8"
 }
 )
+
+
+-- Use Dvorak layout for normal mode keybindings
+vim.cmd([[
+  set langmap='q,\\,w,.e,pr,yt,fy,gu,ci,ro,lp,/[,=],aa,os,ed,uf,ig,dh,hj,tk,nl,s\\;,-',\\;z,qx,jc,kv,xb,bn,mm,w\\,,v.,z/,[-,]=,\"Q,<W,>E,PR,YT,FY,GU,CI,RO,LP,?{,+},AA,OS,ED,UF,IG,DH,HJ,TK,NL,S:,_\",:Z,QX,JC,KV,XB,BN,MM,W<,V>,Z?
+]])
