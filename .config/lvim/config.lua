71489c4d31721197616283a35bb77aa3dd4eef6f
Marco Huamani <mhcaballero79@gmail.com>
2023-09-27 20:38:13 -0300

    [nvim] Change Dvorak

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -193,6 +193,6 @@ vim.api.nvim_create_autocmd("BufEnter", {
 
 
 -- Use Dvorak layout for normal mode keybindings
-vim.cmd([[
-  set langmap='q,\\,w,.e,pr,yt,fy,gu,ci,ro,lp,/[,=],aa,os,ed,uf,ig,dh,hj,tk,nl,s\\;,-',\\;z,qx,jc,kv,xb,bn,mm,w\\,,v.,z/,[-,]=,\"Q,<W,>E,PR,YT,FY,GU,CI,RO,LP,?{,+},AA,OS,ED,UF,IG,DH,HJ,TK,NL,S:,_\",:Z,QX,JC,KV,XB,BN,MM,W<,V>,Z?
-]])
+-- vim.cmd([[
+  -- set langmap='q,\\,w,.e,pr,yt,fy,gu,ci,ro,lp,/[,=],aa,os,ed,uf,ig,dh,hj,tk,nl,s\\;,-',\\;z,qx,jc,kv,xb,bn,mm,w\\,,v.,z/,[-,]=,\"Q,<W,>E,PR,YT,FY,GU,CI,RO,LP,?{,+},AA,OS,ED,UF,IG,DH,HJ,TK,NL,S:,_\",:Z,QX,JC,KV,XB,BN,MM,W<,V>,Z?
+-- ]])
