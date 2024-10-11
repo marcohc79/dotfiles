04e8bff32741b5d0315dfb5bf801bbb093516bdf
Marco Huamani <mhcaballero79@gmail.com>
2024-10-10 22:58:26 -0300

    [nvim] configue again

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,28 +1,28 @@
 return {
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            require("catppuccin").setup({
-                integrations = {
-                    cmp = true,
-                    gitsigns = true,
-                    nvimtree = true,
-                    treesitter = true,
-                    notify = false,
-                    mini = {
-                        enabled = true,
-                        indentscope_color = "",
-                    },
-                },
-            })
-        end,
-    },
-    {
-        "LazyVim/LazyVim",
-        opts = {
-            colorscheme = "catppuccin-mocha",
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          treesitter = true,
+          notify = false,
+          mini = {
+            enabled = true,
+            indentscope_color = "",
+          },
         },
+      })
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
     },
+  },
 }
--- .config/nvim/lua/plugins/java/init.lua
+++ .config/nvim/lua/plugins/java/init.lua
@@ -1,80 +1,70 @@
 return {
-    "nvim-java/nvim-java",
-    config = false,
-    dependencies = {
-        "MunifTanjim/nui.nvim",
-        "mfussenegger/nvim-dap",
-        {
-            "williamboman/mason.nvim",
-            opts = {
-                registries = {
-                    "github:nvim-java/mason-registry",
-                    "github:mason-org/mason-registry",
-                },
-            },
-        },
-        {
-            "neovim/nvim-lspconfig",
-            opts = {
-                servers = {
-                    jdtls = {
-                        handlers = {
-                            ["$/progress"] = function() end,
-                        },
-                        capabilities = vim.lsp.protocol.make_client_capabilities(),
+  "nvim-java/nvim-java",
+  config = false,
+  dependencies = {
+    {
+      "neovim/nvim-lspconfig",
+      opts = {
+        servers = {
+          jdtls = {
+            settings = {
+              java = {
+                configuration = {
+                  runtimes = {
+                    {
+                      name = "Java-21",
+                      path = "/usr/bin/java",
+                      default = true,
                     },
+                  },
                 },
-                setup = {
-                    jdtls = function()
-                        require("java").setup({
-                            -- Archivos que marcan la raíz del proyecto
-                            root_markers = {
-                                "settings.gradle",
-                                "settings.gradle.kts",
-                                "pom.xml",
-                                "build.gradle",
-                                "mvnw",
-                                "gradlew",
-                                "build.gradle",
-                                "build.gradle.kts",
-                                ".git",
-                            },
-                            project_name_fallback = "DefaultJavaProject",
-                            -- Carga de plugins para pruebas en Java
-                            java_test = {
-                                enable = true,
-                            },
-
-                            -- Carga de plugins para el depurador de Java
-                            java_debug_adapter = {
-                                enable = true,
-                            },
-
-                            -- Soporte para herramientas de Spring Boot
-                            spring_boot_tools = {
-                                enable = true,
-                            },
-
-                            -- Instalación automática del JDK usando mason.nvim
-                            jdk = {
-                                auto_install = false,
-                            },
-
-                            -- Configuración de notificaciones, incluyendo las de DAP
-                            notifications = {
-                                dap = true,
-                            },
-
-                            -- Verificaciones de configuración para asegurar la ejecución correcta
-                            verification = {
-                                invalid_order = true,
-                                duplicate_setup_calls = true,
-                                invalid_mason_registry = true,
-                            },
-                        })
-                    end,
-                },
+              },
             },
+          },
         },
+        setup = {
+          jdtls = function()
+            -- Derivar dinámicamente el nombre del proyecto desde el directorio actual
+            local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+
+            require("java").setup({
+              root_markers = {
+                "settings.gradle",
+                "settings.gradle.kts",
+                "pom.xml",
+                "build.gradle",
+                "mvnw",
+                "gradlew",
+                "build.gradle",
+                "build.gradle.kts",
+              },
+              -- Configuración con el nombre dinámico del proyecto
+              project_name = project_name,
+
+              spring_boot_tools = {
+                enable = true,
+              },
+
+              -- Instalación automática del JDK usando mason.nvim
+              jdk = {
+                auto_install = false,
+              },
+
+              -- Configuración de notificaciones, incluyendo las de DAP
+              notifications = {
+                dap = true,
+              },
+
+              -- Verificaciones de configuración para asegurar la ejecución correcta
+              verification = {
+                invalid_order = true,
+                duplicate_setup_calls = true,
+                invalid_mason_registry = true,
+              },
+            })
+          end,
+        },
+      },
     },
+  },
 }
