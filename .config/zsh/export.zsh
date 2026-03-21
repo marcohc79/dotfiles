df65c64b5c1a47b4933cf06f4417f56b1e3ea045
Marco Huamani <mcaballero79@pm.me>
2026-03-20 23:37:17 -0300

    Update kitty, ohmyposh, yazi and zsh.

--- .config/zsh/export.zsh
+++ .config/zsh/export.zsh
@@ -2,6 +2,7 @@
 export AMD_VULKAN_ICD=RADV
 export RADV_PERFTEST=gpl
 export QT_QPA_PLATFORMTHEME=qt6ct
+
 ##### PATH Configuration
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/.cargo/bin:$PATH"
